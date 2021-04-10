3df2c40b4a94eca98b4627bc63c146e92b1bbb7a
perfectliar <z5196167@ad.unsw.edu.au>
2021-04-11 04:33:30 +1000

    ch(deploy): Setup deploy to heroku

--- service/settings.py
+++ service/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from django.db.backends.mysql.base import DatabaseWrapper
+import django_heroku
 
 DatabaseWrapper.data_types['DateTimeField'] = 'datetime'  # fix for MySQL 5.5
 
@@ -159,3 +160,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
